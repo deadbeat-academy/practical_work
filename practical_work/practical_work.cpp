ca9567b1f7bd59564712b46f5d57f5dd28b1ba09
Igor <ig.zayakin@mail.ru>
2024-09-04 19:25:43 +0300

    Добавьте файлы проекта.

---
+++ practical_work/practical_work.cpp
@@ -0,0 +1,21 @@
+﻿// practical_work.cpp
+
+#include <iostream>
+#include<windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    cout << " > Приветствую в лучшем калькуляторе на планете!\n > Выберите цифру нужной функции:";
+    cout << R"(
+    1. Сложение
+    2. Вычитание
+    3. Умножение
+    4. Деление
+ > )" << endl;
+
+    system("pause");
+}
